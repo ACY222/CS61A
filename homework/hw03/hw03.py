5bede2afdf377d52172bdb0e262229c8661c5b40
ACY222 <838113915@qq.com>
2025-06-12 10:32:43 +0800

    finish CS61A

---
+++ homework/hw03/hw03.py
@@ -0,0 +1,273 @@
+HW_SOURCE_FILE = __file__
+
+
+def num_eights(n):
+    """Returns the number of times 8 appears as a digit of n.
+
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
+    8
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
+    2
+    >>> num_eights(12345)
+    0
+    >>> num_eights(8782089)
+    3
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_eights',
+    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr', 'For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 0:
+        return 0
+    if n % 10 == 8:
+        return 1 + num_eights(n // 10)
+    else:
+        return num_eights(n // 10)
+
+
+def digit_distance(n):
+    """Determines the digit distance of n.
+
+    >>> digit_distance(3)
+    0
+    >>> digit_distance(777) # 0 + 0
+    0
+    >>> digit_distance(314) # 2 + 3
+    5
+    >>> digit_distance(31415926535) # 2 + 3 + 3 + 4 + ... + 2
+    32
+    >>> digit_distance(3464660003)  # 1 + 2 + 2 + 2 + ... + 3
+    16
+    >>> from construct_check import check
+    >>> # ban all loops
+    >>> check(HW_SOURCE_FILE, 'digit_distance',
+    ...       ['For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    # Or we can do it like this
+    if n < 10:
+        return 0
+    return abs(n % 10 - n // 10 % 10) + digit_distance(n // 10)
+
+    # my method
+    if n // 10 == 0:
+        return 0
+    last, n, second_last = n % 10, n // 10, n % 100 // 10
+    return digit_distance(n) + abs(last - second_last)
+
+
+def interleaved_sum(n, odd_func, even_func):
+    """Compute the sum odd_func(1) + even_func(2) + odd_func(3) + ..., up
+    to n.
+
+    >>> identity = lambda x: x
+    >>> square = lambda x: x * x
+    >>> triple = lambda x: x * 3
+    >>> interleaved_sum(5, identity, square) # 1   + 2*2 + 3   + 4*4 + 5
+    29
+    >>> interleaved_sum(5, square, identity) # 1*1 + 2   + 3*3 + 4   + 5*5
+    41
+    >>> interleaved_sum(4, triple, square)   # 1*3 + 2*2 + 3*3 + 4*4
+    32
+    >>> interleaved_sum(4, square, triple)   # 1*1 + 2*3 + 3*3 + 4*3
+    28
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'interleaved_sum', ['While', 'For', 'Mod']) # ban loops and %
+    True
+    >>> check(HW_SOURCE_FILE, 'interleaved_sum', ['BitAnd', 'BitOr', 'BitXor']) # ban bitwise operators, don't worry about these if you don't know what they are
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    # a better method
+    def sum_from(k):
+        if k > n:
+            return 0
+        elif k == n:
+            return odd_func(k)
+        else:
+            return odd_func(k) + even_func(k+1) + odd_func(k+2)
+    return sum_from(1)
+    # my method
+    if n == 0:
+        return 0
+    if n % 10 in [0, 2, 4, 6, 8]:   # if n is odd
+        return interleaved_sum(n - 1, odd_func, even_func) + even_func(n)
+    else:
+        return interleaved_sum(n - 1, odd_func, even_func) + odd_func(n)
+    
+
+
+def next_smaller_dollar(bill):
+    """Returns the next smaller bill in order."""
+    if bill == 100:
+        return 50
+    if bill == 50:
+        return 20
+    if bill == 20:
+        return 10
+    elif bill == 10:
+        return 5
+    elif bill == 5:
+        return 1
+
+def count_dollars(total):
+    """Return the number of ways to make change.
+
+    >>> count_dollars(15)  # 15 $1 bills, 10 $1 & 1 $5 bills, ... 1 $5 & 1 $10 bills
+    6
+    >>> count_dollars(10)  # 10 $1 bills, 5 $1 & 1 $5 bills, 2 $5 bills, 10 $1 bills
+    4
+    >>> count_dollars(20)  # 20 $1 bills, 15 $1 & $5 bills, ... 1 $20 bill
+    10
+    >>> count_dollars(45)  # How many ways to make change for 45 dollars?
+    44
+    >>> count_dollars(100) # How many ways to make change for 100 dollars?
+    344
+    >>> count_dollars(200) # How many ways to make change for 200 dollars?
+    3274
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_dollars', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    # [1, 5, 10, 20, 50, 100]
+        # current_bill indicates the bill with the largest value
+    def helper_counter(total, current_bill):
+        # count the ways to partition total using parts up to current_bill
+        if total < 0:
+            return 0
+        elif total == 0 or current_bill == 1:
+            return 1
+        # use current_bill + don't use current_bill
+        with_current_bill = helper_counter(total - current_bill, current_bill)
+        without_current_bill = helper_counter(total, next_smaller_dollar(current_bill))
+        return with_current_bill + without_current_bill
+    return helper_counter(total, 100)
+
+
+
+def next_larger_dollar(bill):
+    """Returns the next larger bill in order."""
+    if bill == 1:
+        return 5
+    elif bill == 5:
+        return 10
+    elif bill == 10:
+        return 20
+    elif bill == 20:
+        return 50
+    elif bill == 50:
+        return 100
+
+def count_dollars_upward(total):
+    """Return the number of ways to make change using bills.
+
+    >>> count_dollars_upward(15)  # 15 $1 bills, 10 $1 & 1 $5 bills, ... 1 $5 & 1 $10 bills
+    6
+    >>> count_dollars_upward(10)  # 10 $1 bills, 5 $1 & 1 $5 bills, 2 $5 bills, 10 $1 bills
+    4
+    >>> count_dollars_upward(20)  # 20 $1 bills, 15 $1 & $5 bills, ... 1 $20 bill
+    10
+    >>> count_dollars_upward(45)  # How many ways to make change for 45 dollars?
+    44
+    >>> count_dollars_upward(100) # How many ways to make change for 100 dollars?
+    344
+    >>> count_dollars_upward(200) # How many ways to make change for 200 dollars?
+    3274
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_dollars_upward', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def helper_counter(total, current_bill):
+        # count the ways to partition total using parts up to current_bill
+        if current_bill == None: # the largest bill is 100
+            return 0
+        if total < current_bill: # cannot combine more smaller bills 
+            return 0
+        if current_bill == 1:
+            return 1 + helper_counter(total, next_larger_dollar(current_bill))
+        # use current_bill + don't use current_bill
+        return 1 + helper_counter(total - current_bill, current_bill) +\
+                helper_counter(total, next_larger_dollar(current_bill))
+    return helper_counter(total, 1)
+
+
+def print_move(origin, destination):
+    """Print instructions to move a disk."""
+    print("Move the top disk from rod", origin, "to rod", destination)
+
+def move_stack(n, start, end):
+    """Print the moves required to move n disks on the start pole to the end
+    pole without violating the rules of Towers of Hanoi.
+
+    n -- number of disks
+    start -- a pole position, either 1, 2, or 3
+    end -- a pole position, either 1, 2, or 3
+
+    There are exactly three poles, and start and end must be different. Assume
+    that the start pole has at least n disks of increasing size, and the end
+    pole is either empty or has a top disk larger than the top n start disks.
+
+    >>> move_stack(1, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    >>> move_stack(2, 1, 3)
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 3
+    >>> move_stack(3, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 3 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 1
+    Move the top disk from rod 2 to rod 3
+    Move the top disk from rod 1 to rod 3
+    """
+    assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        print_move(start, end)
+    else:
+        # a much smarter way
+        pass_by = 6 - start - end
+        # my stupid way
+        """
+        pos = [1, 2, 3]
+        pos.remove(start)
+        pos.remove(end)
+        pass_by = pos[0]
+        """
+        move_stack(n-1, start, pass_by)
+        move_stack(1, start, end)
+        move_stack(n-1, pass_by, end)
+
+
+from operator import sub, mul
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> # ban any assignments or recursion
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial',
+    ...     ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return (lambda f: lambda k: f(f, k))    \
+        (lambda f, k: k if k == 1 else mul(k, f(f, sub(k, 1))))
+
+
